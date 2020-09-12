56ea9b03ea1028a5df4b5c99957137097b5a59a5
Shashank Prasad <skshashankkumar41@gmail.com>
2020-09-13 04:54:01 +0530

    final changes

--- compare_pose.py
+++ compare_pose.py
@@ -9,31 +9,45 @@ from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 fps_time = 0
 
-args= {'resize':'432x368', 'model':'mobilenet_thin','camera':0,'resize_out_ratio':4.0,'vid':"output.avi"}
-w, h = model_wh(args['resize'])
+parser = argparse.ArgumentParser(description='pose-comparator')
+parser.add_argument('--video', type=str, default='output.avi')
+parser.add_argument('--camera', type=int, default=0)
+parser.add_argument('--resize', type=str, default='0x0',
+                    help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
+parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                    help='if provided, resize heatmaps before they are post-processed. default=1.0')
+parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+parser.add_argument('--show-process', type=bool, default=False,
+                    help='for debug purpose, if enabled, speed for inference is dropped.')
+parser.add_argument('--tensorrt', type=str, default="False",
+                    help='for tensorrt process.')
+args = parser.parse_args()
+
+#args= {'resize':'432x368', 'model':'mobilenet_thin','camera':0,'resize_out_ratio':4.0,'video':"output.avi"}
+w, h = model_wh(args.resize)
 
 if w > 0 and h > 0:
-    e = TfPoseEstimator(get_graph_path(args['model']), target_size=(w, h))
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
 else:
-    e = TfPoseEstimator(get_graph_path(args['model']), target_size=(432, 368))
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
 
-cam = cv2.VideoCapture(args['camera'])
+out = False
+cam = cv2.VideoCapture(args.camera)
 ret_val, image = cam.read()
-cam_store = cv2.VideoCapture(args['vid'])
+cam_store = cv2.VideoCapture(args.video)
 ret_val_store, image_store = cam_store.read()
 
-while True:
+while cam_store.isOpened():
     dist = True 
-    
     ret_val_store, image_store = cam_store.read()
-    print("DIST--CHANGED--::::")
+    print("-------Next Pose Applied-------")
     while dist:
         ret_val, image = cam.read()
-        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args['resize_out_ratio'])
+        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
         #print("ZEROS::",image.shape)
 
         image, blackImage = TfPoseEstimator.draw_humans(image, humans, imgcopy=True,black_image = True)
-        # import matplotlib.pyplot as plt
+        # import matplotlib.pyplot as pltqqqqqqqqq
         # plt.imsave('test2.jpg', image, cmap='Greys')
         
         final = cv2.bitwise_or(image, image_store)
@@ -42,20 +56,22 @@ while True:
         # cv2.putText(image,
         #             "FPS: %f" % (1.0 / (time.time() - fps_time)),
         #             (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-        #             (0, 255, 0), 2)
+        #             (0, 255, 0), 2)q
         #cv2.imshow('tf-pose-estimation result', final)
         Aflat = blackImage.flatten(order='C')
         Bflat = image_store.flatten(order='C')
 
         dist = distance.cosine(Aflat, Bflat)
-        print("DIST::",dist)
-        if dist > 0.80:
+        print("Similarity:",dist)
+        if dist > 0.90:
             dist = False
         
         fps_time = time.time()
         if cv2.waitKey(25) & 0xFF == ord('q'): 
+            out = True
             break
-    if cv2.waitKey(25) & 0xFF == ord('q'): 
+            
+    if cv2.waitKey(5) & out: 
             break
 
 cv2.destroyAllWindows()
--- create_pose.py
+++ create_pose.py
@@ -8,26 +8,41 @@ import numpy as np
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 fps_time = 0
-args= {'resize':'432x368', 'model':'mobilenet_thin','camera':0,'resize_out_ratio':4.0}
-w, h = model_wh(args['resize'])
+
+parser = argparse.ArgumentParser(description='pose-creator')
+parser.add_argument('--camera', type=int, default=0)
+parser.add_argument('--resize', type=str, default='0x0',
+                    help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
+parser.add_argument('--resize-out-ratio', type=float, default=4.0,
+                    help='if provided, resize heatmaps before they are post-processed. default=1.0')
+parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+parser.add_argument('--show-process', type=bool, default=False,
+                    help='for debug purpose, if enabled, speed for inference is dropped.')
+parser.add_argument('--path', type=str, default="",
+                    help='for tensorrt process.')
+
+args = parser.parse_args()
+
+#args= {'resize':'432x368', 'model':'mobilenet_thin','camera':0,'resize_out_ratio':4.0}
+w, h = model_wh(args.resize)
 
 if w > 0 and h > 0:
-    e = TfPoseEstimator(get_graph_path(args['model']), target_size=(w, h))
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
 else:
-    e = TfPoseEstimator(get_graph_path(args['model']), target_size=(432, 368))
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
 
-cam = cv2.VideoCapture(args['camera'])
+cam = cv2.VideoCapture(args.camera)
 ret_val, image = cam.read()
 print("IMAGESHAPE:::",image.shape)
-vid_writer = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (image.shape[1],image.shape[0]),True)
+vid_writer = cv2.VideoWriter(args.path+'output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 5, (image.shape[1],image.shape[0]),True)
 while True:
     ret_val, image = cam.read()
     
-    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args['resize_out_ratio'])
+    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
     image[image != 0] = 0
     #print("ZEROS::",image.shape)
     image = TfPoseEstimator.draw_humans(image, humans, imgcopy=True)
-    print("SHAPE::",image.shape)
+    #print("SHAPE::",image.shape)
 
     # cv2.putText(image,
     #             "FPS: %f" % (1.0 / (time.time() - fps_time)),
